0cb1c6a1e191a609553c6c0c3f73c22630dc8aef
Kian <endud5118@gmail.com>
2021-04-27 20:59:17 +0900

    first commit

---
+++ hell.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+/// find out the number that two times
+func main() {
+	var a []int = []int{1, 2, 4, 4, 15, 15, 7, 88, 3}
+
+	for i, element := range a {
+		for j := i + 1; j < len(a); j++ {
+			element2 := a[j]
+			if element2 == element {
+				fmt.Println(element)
+			}
+		}
+	}
+
+	/*
+			for i := 0; i < len(a); i++ {
+				fmt.Println(a[i])
+			}
+
+		for _, element := range a {
+			fmt.Printf("%d\n", element)
+		}*/
+}
